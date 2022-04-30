114a0c38898796cf623647e8b1e31994ca4afc16
MirboboyevJasurjan <MirboboyevJasurjan@gmail.com>
2022-04-30 06:40:22 +0500

    Music Player

---
+++ music-list.js
@@ -0,0 +1,128 @@
+let allMusic = [
+  {
+    name: "Miyai & And Panda –  Патрон",
+    artist: " Hajiime Records",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "Intro Hajime ",
+    artist: "Miyagi",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Нам надо дыма",
+    artist: "Litvinenko",
+    img: "3",
+    src: "3"
+  },
+  {
+    name: "Let me down slowly",
+    artist: "Unknown",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "Мы открываем бизнес",
+    artist: "💋",
+    img: "5",
+    src: "5"
+  },
+  {
+    name: "Моя Банда ",
+    artist: "Hajime Records",
+    img: "6",
+    src: "6"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "7",
+    src: "7"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "8",
+    src: "8"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "9",
+    src: "9"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "10",
+    src: "10"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "11",
+    src: "11"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "12",
+    src: "12"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "13",
+    src: "13"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "14",
+    src: "14"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "15",
+    src: "15"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "16",
+    src: "16"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "17",
+    src: "17"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "18",
+    src: "18"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "19",
+    src: "19"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "20",
+    src: "20"
+  },
+  {
+    name: "Unicum Beats ",
+    artist: "Unicum Records",
+    img: "21",
+    src: "21"
+  },
+];
